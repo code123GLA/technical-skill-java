24d3eb1da26808e81a80619d5e6f47bbd24c52d7
Gaurav sharma <gsharma25112007@gmail.com>
2026-08-22 12:57:59 +0530

    Add files via upload

---
+++ ReplaceVowel.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReplaceVowel {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String str = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+                
+                str = str.substring(0, i) + "*" + str.substring(i + 1);
+            }
+        }
+
+        System.out.println("After replacing vowels: " + str);
+    }
+}
